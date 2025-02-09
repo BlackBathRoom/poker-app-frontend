8351a24a08140d6a9fc5c93cde9560d8dfcaa015
InoueToranosuke <ktc24a31e0002@edu.kyoto-tech.ac.jp>
2025-02-10 00:23:20 +0900

    GamecontrolConpornent

---
+++ src/components/Admin/GameControl.tsx
@@ -0,0 +1,24 @@
+import { useModal } from "../Modal/useModal";
+import ModalFrame from "../Modal/ModalFrame";
+import GameStartBtn from "../GameCtlBtn/GameStartBtn";
+import GameEndBtn from "../GameCtlBtn/GameEndBtn";
+
+const GameControl = () => {
+  const { Modal, openModal, closeModal } = useModal();
+
+  return (
+    <>
+      <button onClick={openModal} className="bg-blue-600 px-4 py-2 text-white rounded-md">
+        ゲーム制御
+      </button>
+      <Modal>
+        <ModalFrame modalName="ゲーム管理" closeModal={closeModal}>
+          <GameStartBtn startGame={() => alert("ゲーム開始！")} />
+          <GameEndBtn userNames={["Player1", "Player2"]} endGame={(index) => alert(`ゲーム終了: ${index}`)} />
+        </ModalFrame>
+      </Modal>
+    </>
+  );
+};
+
+export default GameControl;
