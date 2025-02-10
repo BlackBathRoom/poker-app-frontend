d7e981dfc5c6486f7a38ce29eff4d9de7b7f9105
[itsuki0016] <[ktc24a31e0016@edu.kyoto-tech.ac.jp]>
2025-02-11 02:33:25 +0900

    deleteUserid try >> then

--- src/api/Userid/deleteUserid.ts
+++ src/api/Userid/deleteUserid.ts
@@ -2,14 +2,11 @@ import axios from "axios";
 import { API_BASE_URL } from "../../config";
 
 export const deleteUserid = async (): Promise<boolean> => {
-	try{
-		await axios.delete(API_BASE_URL);
-		return true;
-	}
-	catch (error) {
-		console.error("ユーザーIDの削除に失敗しました:", error);
-		return false;
-	}
-
+    return axios
+        .delete(API_BASE_URL)
+        .then(() => true)
+        .catch((error) => {
+            console.error("ユーザーIDの削除に失敗しました:", error);
+            return false;
+        });
 };
-
