a895f34beb72f917febbdfdc4c78e2644b05634d
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-11 01:37:40 +0900

    レビューコメントを追加

--- src/api/Userid/putUserid.ts
+++ src/api/Userid/putUserid.ts
@@ -5,9 +5,10 @@ const API_URL = `${API_BASE_URL}/users`;
 
 export const putUserid = async (
 	userId: string,
+	// UserInfo型をOptionalにして受け取るように
 	userData: Partial<{name: string; chip:number}>
-):
-	Promise<boolean> => {
+): Promise<boolean> => {
+	// エラーハンドリングはtry-catchでじゃなくて.then、.catchの方がいいかも
 	try{
 		await axios.put(`${API_URL}/${userId}`, userData);
 		return true;
