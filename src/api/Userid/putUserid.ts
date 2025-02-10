523197dad24a0bbfdfdbc18f5cb492ebb7607c95
[itsuki0016] <[ktc24a31e0016@edu.kyoto-tech.ac.jp]>
2025-02-11 02:56:26 +0900

    clean getUserid

--- src/api/Userid/putUserid.ts
+++ src/api/Userid/putUserid.ts
@@ -3,19 +3,16 @@ import { API_BASE_URL } from "../../config";
 
 const API_URL = `${API_BASE_URL}/users`;
 
-export const putUserid = async (
-	userId: string,
-	// UserInfo型をOptionalにして受け取るように
-	userData: Partial<{name: string; chip:number}>
-): Promise<boolean> => {
-	// エラーハンドリングはtry-catchでじゃなくて.then、.catchの方がいいかも
-	try{
-		await axios.put(`${API_URL}/${userId}`, userData);
-		return true;
-	}
-	catch (error) {
-		console.error("ユーザー情報の更新に失敗しました:", error);
-		return false;
-	}
-};
 
+export const putUserid = async (
+    userId: string,
+    userData: Partial<{ name: string; chip: number }>
+): Promise<boolean> => {
+    return axios
+        .put(`${API_URL}/${userId}`, userData)
+        .then(() => true)
+        .catch((error) => {
+            console.error("ユーザー情報の更新に失敗しました:", error);
+            return false;
+        });
+};
