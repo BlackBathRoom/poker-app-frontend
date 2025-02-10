9b21c6d8e443bc5f1c453e1b517f4cdff6ee6239
[itsuki0016] <[ktc24a31e0016@edu.kyoto-tech.ac.jp]>
2025-02-11 02:27:34 +0900

    getUserid.ts try >> then

--- src/api/Uproperty/getUseridproperty.ts
+++ src/api/Uproperty/getUseridproperty.ts
@@ -1,3 +1,16 @@
 import axios from "axios";
+import { API_BASE_URL } from "../../config";
 
-const 
+const API_URL = `${API_BASE_URL}/users`;
+
+export const getUserproperty = async (
+    userId: string
+): Promise<{ name?: string; chip?: number } | null> => {
+    return axios
+        .get<{ name?: string; chip?: number }>(`${API_URL}/${userId}`)
+        .then((response) => response.data)
+        .catch((error) => {
+            console.error("ユーザーのプロパティの取得に失敗しました:", error);
+            return null;
+        });
+};
