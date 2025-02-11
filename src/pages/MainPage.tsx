a9150963db6ec5c610b65beec717a88d41fd1296
haradatomoki0512 <ktc24a31e0012@edu.kyoto-tech.ac.jp>
2025-02-12 04:07:25 +0900

    コンポーネント分割

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,6 +3,7 @@ import ActionBtn from "../components/UserManage/ActionBtn";
 import ActionModal from "../components/UserManage/ActionModal/ActionModal";
 import type { ActionType } from "../game/types";
 import GameInfo from "../components/GameInfo/GameInfo";
+import UserInfo from "../components/Userinformation/UserInfo"; 
 import { useState } from "react";
 
 const MainPage: React.FC = () => {
@@ -24,15 +25,7 @@ const MainPage: React.FC = () => {
                     <div className="mt-4 sm:mt-6 md:mt-8 lg:mt-10">
                         <GameInfo potSize={256} rate={25600} />
                     </div>
-                    <div className="mt-6 sm:mt-8 md:mt-10 lg:mt-12">
-                        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">プレイヤー情報</h2>
-                        <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-                            ユーザー名: <span className="font-bold text-indigo-600">{userName}</span>
-                        </p>
-                        <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-                            チップ数: <span className="font-bold text-green-500">{chips} 枚</span>
-                        </p>
-                    </div>
+                    <UserInfo userName={userName} chips={chips} />
                     <div className="w-full flex justify-center">
                         <ActionBtn handleModal={openModal} isPlaying={true} />
                     </div>
