6b2f6fe725c88eaa427275a1a0de6abaa8c5b4c9
InoueToranosuke <ktc24a31e0002@edu.kyoto-tech.ac.jp>
2025-02-12 04:33:26 +0900

    img white

--- src/components/Admin/UserEdit.tsx
+++ src/components/Admin/UserEdit.tsx
@@ -7,7 +7,7 @@ const UserEdit = () => {
   return (
     <>
       <img 
-        src="/Edit.svg"  
+        src="/Edit_w.svg"  
         alt="ユーザー情報変更"
         onClick={openModal} 
         className="fill-white w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 cursor-pointer "
