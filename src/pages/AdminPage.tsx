d75b79e7b6b3381380cafbb5c6dbc3ede32e4638
InoueToranosuke <ktc24a31e0002@edu.kyoto-tech.ac.jp>
2025-02-11 16:58:00 +0900

    データ入力例

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import UserList from "../components/Admin/UserList";
 import GameControl from "../components/Admin/GameControl";
-import UserEdit from "../components/Admin/UserEdit";
 
 type User = {
   id: number;
@@ -10,22 +9,23 @@ type User = {
 };
 
 const AdminPage: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<User[]>([
+    { id: 1, name: "SampleUser", chips: 1000 }
+  ]);
 
-  useEffect(() => {
-    fetch("/api/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api/users")
+  //     .then((res) => res.json())
+  //     .then((data) => setUsers(data));
+  // }, []);
 
   return (
     <div className="p-10 space-y-4">
-      <h1 className="text-2xl font-bold text-black text-center ">管理者ページ</h1>
+      <h1 className="text-2xl font-bold text-black text-center">管理者ページ</h1>
       <UserList users={users} />
       <GameControl />
-      <UserEdit />
     </div>
   );
-}
+};
 
 export default AdminPage;
