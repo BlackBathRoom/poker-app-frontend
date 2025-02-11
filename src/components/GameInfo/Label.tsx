ae6fe455c840ddec8a8433622a887d16524346a3
haradatomoki0512 <ktc24a31e0012@edu.kyoto-tech.ac.jp>
2025-02-12 04:26:36 +0900

    レスポンシブ対応label

--- src/components/GameInfo/Label.tsx
+++ src/components/GameInfo/Label.tsx
@@ -6,8 +6,8 @@ type Props = {
 const Label: React.FC<Props> = ({ label, value }) => {
     return (
         <div className="flex flex-col gap-2 justify-center items-center p-2">
-            <span className="text-5xl font-bold">{label}</span>
-            <span className="text-6xl font-bold">{value}</span>
+            <span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">{label}</span>
+            <span className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">{value}</span>
         </div>
     );
 };
