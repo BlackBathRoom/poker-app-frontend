d605db6311e92a02cc9f19a91d181e8f898d4ffa
haradatomoki0512 <ktc24a31e0012@edu.kyoto-tech.ac.jp>
2025-02-13 00:38:24 +0900

    flex使った使った

--- src/components/Userinformation/UserInfo.tsx
+++ src/components/Userinformation/UserInfo.tsx
@@ -1,14 +1,12 @@
 interface UserInfoProps {
     userName: string;
     chips: number;
-    // isPlayingに変更
-    isParticipating?: boolean;  // 追加（オプションにする） <- 何で？
+    isParticipating: boolean;
 }
 
-const UserInfo: React.FC<UserInfoProps> = ({ userName, chips, isParticipating = true }) => {  // デフォルト値を設定
+const UserInfo: React.FC<UserInfoProps> = ({ userName, chips, isParticipating }) => { 
     return (
-        // flexコンテナーにしてgap指定で間隔をとる
-        <div className="">
+        <div className="flex flex-col gap-4 items-center">
             <p className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl">
                 <span className="font-bold text-indigo-600">{userName}</span>
             </p>
