eae365a86bea2dfef0199a8dd694efd4adb8c7e7
haradatomoki0512 <ktc24a31e0012@edu.kyoto-tech.ac.jp>
2025-02-12 16:28:48 +0900

    参加不参加追加

--- src/components/Userinformation/UserInfo.tsx
+++ src/components/Userinformation/UserInfo.tsx
@@ -1,13 +1,17 @@
 interface UserInfoProps {
     userName: string;
     chips: number;
+    isParticipating?: boolean;  // 追加（オプションにする）
 }
 
-const UserInfo: React.FC<UserInfoProps> = ({ userName, chips }) => {
+const UserInfo: React.FC<UserInfoProps> = ({ userName, chips, isParticipating = true }) => {  // デフォルト値を設定
     return (
         <div className="">
             <p className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl">
-                ユーザー名: <span className="font-bold text-indigo-600">{userName}</span>
+                <span className="font-bold text-indigo-600">{userName}</span>
+            </p>
+            <p className={`text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold ${isParticipating ? 'text-green-500' : 'text-red-500'}`}>
+                {isParticipating ? '参加中' : '不参加'}
             </p>
             <p className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl">
                 チップ数: <span className="font-bold text-green-500">{chips} 枚</span>
