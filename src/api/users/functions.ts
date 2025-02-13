533a9da601a785be4b6eb6e4ab8daf558b534da8
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-13 09:40:40 +0900

    データ処理系の関数を作成

---
+++ src/api/users/functions.ts
@@ -0,0 +1,96 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { API_BASE_URL } from "../../config";
+import type { Role, UserInfo } from "../../game/types"
+import type { UserData } from "./types";
+
+
+const URL = `${API_BASE_URL}/users`;
+
+type GetResponse<T> = T extends string ? UserData : UserData[];
+
+const _fetchUserInfo = async (userId?: string): Promise<UserData[]> => {
+    const url = `${URL}/${userId? userId : ""}`;
+    const options: AxiosRequestConfig = {
+        url,
+        method: "GET"
+    };
+
+    return await axios(options)
+        .then((res: AxiosResponse<GetResponse<typeof userId>>) => {
+            if (Array.isArray(res.data)) {
+                return res.data;
+            } else {
+                return [res.data];
+            }
+        })
+        .catch((err) => {
+            throw new Error(err);
+        });
+};
+
+export const fetchUserInfo = async (userId: string): Promise<UserData> => {
+    const data = await _fetchUserInfo(userId);
+    return await data[0];
+};
+
+export const fetchAllUserInfo = async (): Promise<UserData[]> => {
+    return await _fetchUserInfo();
+};
+
+
+type PostResponse = {
+    id: string;
+}
+
+export const postUserInfo = async (userInfo: UserInfo): Promise<string> => {
+    const data: UserData = {
+        name: userInfo.name,
+        chip: userInfo.chip,
+        role: userInfo.role === "" ? null : userInfo.role,
+        isplaying: userInfo.isPlaying,
+    };
+
+    const options: AxiosRequestConfig = {
+        url: `${API_BASE_URL}/users/`, 
+        method: "POST",
+        data,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }
+
+    return await axios(options)
+        .then((res: AxiosResponse<PostResponse>) => {
+            return res.data.id;
+        })
+        .catch((err) => {
+            throw new Error(err);
+        });
+};
+
+
+const _updateUserInfo = async (
+    userId: string,
+    data: Partial<Pick<UserData, "chip" | "role" | "isplaying">>,
+    resource: string,
+): Promise<void> => {
+    const url = `${URL}/${userId}/${resource}`;
+    const options: AxiosRequestConfig = {
+        url,
+        method: "PUT",
+        data,
+    };
+    await axios(options);
+};
+
+export const updateChip = async (userId: string, chip: number): Promise<void> => {
+    await _updateUserInfo(userId, { chip }, "chip");
+};
+
+export const updateRole = async (userId: string, role: Role): Promise<void> => {
+    await _updateUserInfo(userId, { role: role ? role : null }, "role");
+};
+
+export const updateIsPlaying = async (userId: string, isPlaying: boolean): Promise<void> => {
+    await _updateUserInfo(userId, { isplaying: isPlaying }, "isplaying");
+};
