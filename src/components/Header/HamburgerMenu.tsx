70698e3cd4511c8c23d86f1b1d765ea95cd7db9b
InoueToranosuke <ktc24a31e0002@edu.kyoto-tech.ac.jp>
2025-02-13 18:31:23 +0900

    rounded-full

--- src/components/Header/HamburgerMenu.tsx
+++ src/components/Header/HamburgerMenu.tsx
@@ -10,13 +10,13 @@ const HamburgerMenu = () => {
         className="fixed top-4 right-4 w-8 h-6 bg-transparent border-none outline-none cursor-pointer z-10"
       >
         <span
-          className={`block w-8 h-1 bg-white mb-2 transition-all duration-300 ${isOpen ? 'transform rotate-45 translate-y-4' : ''}`}
+          className={`block w-8 h-1 bg-white mb-2 transition-all duration-300 rounded-full ${isOpen ? 'transform rotate-45 translate-y-4' : ''}`}
         ></span>
         <span
-          className={`block w-8 h-1 bg-white mb-2 transition-all duration-300 ${isOpen ? 'opacity-0' : ''}`}
+          className={`block w-8 h-1 bg-white mb-2 transition-all duration-300 rounded-full ${isOpen ? 'opacity-0' : ''}`}
         ></span>
         <span
-          className={`block w-8 h-1 bg-white transition-all duration-300 ${isOpen ? 'transform -rotate-45 -translate-y-2' : ''}`}
+          className={`block w-8 h-1 bg-white transition-all duration-300 rounded-full ${isOpen ? 'transform -rotate-45 -translate-y-2' : ''}`}
         ></span>
       </button>
 
