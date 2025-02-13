53886bf5615cc13675503ff4fef72c9ef42d8bd5
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-13 13:34:08 +0900

    ActionModalからのデータ更新を組み込み

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,12 +2,13 @@ import { useModal } from "../components/Modal/useModal";
 import { useNavigate } from "react-router-dom";
 
 import { useUserContext } from "../hook/useUserContext";
+import type { ActionType } from "../game/types";
+import { useGetUser, usePutUserInfo } from "../api/users";
+import { useGame } from "../hook/useGame";
 import ActionBtn from "../components/UserManage/ActionBtn";
 import ActionModal from "../components/UserManage/ActionModal/ActionModal";
-import type { ActionType } from "../game/types";
 import GameInfo from "../components/GameInfo/GameInfo";
 import UserInfo from "../components/Userinformation/UserInfo";
-import { useGetUser } from "../api/users";
 
 
 const MainPage: React.FC = () => {
@@ -18,11 +19,19 @@ const MainPage: React.FC = () => {
     if (id === null) navigate("/login", { replace: true });
 
     const { data, isPending, isError } = useGetUser(id as string);
+    const mutate = usePutUserInfo(id as string);
 
     const { Modal, openModal, closeModal } = useModal();
+    const game = useGame();
 
-    const dummyAction = (actionType: ActionType, chip?: number) => {
-        console.log(actionType, chip);
+    const action = (actionType: ActionType, amount: number) => {
+        if (!data) throw new Error("ユーザー情報が取得できません");
+       const update = game.action(
+            actionType,
+            { rate: 3, potSize: 3 },
+            { chip: data.chip, amount },
+        );
+        mutate.mutate(update.userInfo);
     };
 
     if (isPending) return <div>Loading...</div>;
@@ -33,7 +42,7 @@ const MainPage: React.FC = () => {
         <div className="h-full w-full flex flex-col items-center justify-center">
             <div className="flex flex-col items-center justify-center flex-grow w-full">
                 <Modal>
-                    <ActionModal action={dummyAction} closeModal={closeModal} />
+                    <ActionModal action={action} closeModal={closeModal} />
                 </Modal>
                 <div className="bg-white text-gray-900 p-8 sm:p-10 md:p-12 rounded-lg shadow-md text-center 
                                 w-[90%] sm:max-w-[600px] md:max-w-[700px] lg:max-w-[850px] min-h-[75vh] 
