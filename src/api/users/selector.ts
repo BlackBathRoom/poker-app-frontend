c1a0ab671bd3c62d042606e0861d5f52e259668a
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-13 09:46:29 +0900

    userInfo変換用の関数を作成

---
+++ src/api/users/selector.ts
@@ -0,0 +1,12 @@
+import type { UserInfo } from "../../game/types";
+import type { UserData } from "./types";
+
+export const userInfoSelector = (data: UserData): UserInfo => {
+    const info: UserInfo = {
+        name: data["name"],
+        chip: data["chip"],
+        role: data["role"] === null ? "" : data["role"],
+        isPlaying: data["isplaying"],
+    };
+    return info;
+};
