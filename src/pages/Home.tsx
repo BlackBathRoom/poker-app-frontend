a43bef7347f71186d1362b1b112e3edc24b3535b
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-14 04:36:25 +0900

    homeでゲーム情報をフェッチ

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,35 @@
+import { useGetGameInfo } from "../api/gameInfo";
 import { useGetAllUser } from "../api/users";
 import GameInfo from "../components/GameInfo/GameInfo";
+import { FIXED_GAME_ID } from "../config";
 
 
 const Home: React.FC = () => {
-    const { data, isPending, isError } = useGetAllUser();
+    const userQuery = useGetAllUser();
+    const gameInfoQuery = useGetGameInfo(FIXED_GAME_ID);
 
-    if (isPending) {
+    if (userQuery.isPending || gameInfoQuery.isPending) {
         return <div>Loading...</div>;
     }
-    if (isError) {
+    if (userQuery.isError || gameInfoQuery.isError) {
         return <div>Error</div>;
     }
-    if (!data) {
+    if (!userQuery.data || !gameInfoQuery.data) {
         return <div>No data</div>;
     }
 
     return (
         <div className="w-full max-w-screen-md h-full m-auto flex flex-col gap-10">
             <div className="h-1/4">
-                <GameInfo potSize={0} rate={0} />
+                <GameInfo potSize={gameInfoQuery.data.pot} rate={gameInfoQuery.data.currentBet} />
             </div>
             <div className="min-h-[22rem] flex bg-green-100 border border-slate-950 rounded-md">
                 <div className="px-2 py-3 h-full m-auto ">
                     <ul className="flex flex-col gap-3 h-full w-72 justify-center items-center">
-                        {data.map((userInfo, index) => (
+                        {userQuery.data.map((userInfo, index) => (
                             <li
                             key={index}
-                            className={`w-5/6 flex flex-col gap-3 ${(data.length - 1) !== index && (
-                                'after:inline-block after:w-full after:h-[0.8px] after:bg-slate-950')
-                            }`}>
+                            className="w-5/6 flex flex-col gap-3">
                                 <div className="flex justify-between text-3xl">
                                     <span>{userInfo.name}</span>
                                     <span>{userInfo.chip}</span>
