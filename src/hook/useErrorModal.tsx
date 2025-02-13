0778d49a8fcee78349d3170e595b2918f28f8c02
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-13 13:24:26 +0900

    useErrorModalを作成

--- src/hook/useErrorModal.tsx
+++ src/hook/useErrorModal.tsx
@@ -1,43 +1,6 @@
-import { createContext, useState } from "react";
-import { useModal } from "../components/Modal/useModal";
-import ModalFrame from "../components/Modal/ModalFrame";
+import { errorModalContext, ErrorModalContextTyep } from "../context/errorModalContext"
+import { useContextClient } from "./useContextClient"
 
-export const openErrorModalContext = createContext(
-    {} as (error: Error) => void
-);
-
-type ModalProps = {
-    children: React.ReactNode;
-};
-
-export const useErrorModal = () => {
-    const { Modal, openModal, closeModal } = useModal();
-    const [error, setError] = useState<Error | null>(null);
-    
-    const openErrorModal = (error: Error) => {
-        setError(error);
-        openModal();
-    };
-
-    const ErrorModal: React.FC = () => {
-        return (
-            <Modal>
-                <ModalFrame modalName="Oops!" closeModal={closeModal}>
-                    <span className="text-rose-400 text-3xl font-extrabold">
-                        {error?.message}
-                    </span>
-                </ModalFrame>
-            </Modal>
-        );
-    };
-
-    const OpenErrorModalProvider: React.FC<ModalProps> = ({ children }) => {
-        return (
-            <openErrorModalContext.Provider value={openErrorModal}>
-                {children}
-            </openErrorModalContext.Provider>
-        );
-    };
-
-    return { ErrorModal, OpenErrorModalProvider };
+export const useErrorModal = (): ErrorModalContextTyep => {
+    return useContextClient(errorModalContext);
 };
