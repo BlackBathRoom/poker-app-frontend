00d29f8f7ce903480b5011341c4c5af665c09a2b
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-13 13:24:03 +0900

    sestErrorの型を変更

--- src/context/errorModalContext.tsx
+++ src/context/errorModalContext.tsx
@@ -1,8 +1,8 @@
 import { createContext } from "react";
 
 export type ErrorModalContextTyep = {
-    setError: React.Dispatch<React.SetStateAction<string | null>>;
+    setError: React.Dispatch<React.SetStateAction<Error | null>>;
     openErrorModal: (error: Error) => void;
 }
 
-export const openErrorModalContext = createContext<ErrorModalContextTyep | undefined>(undefined);
+export const errorModalContext = createContext<ErrorModalContextTyep | undefined>(undefined);
