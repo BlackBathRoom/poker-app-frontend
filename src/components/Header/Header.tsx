fa4a48b28131ebadd5c6240f4e96d64180b1cb6f
InoueToranosuke <ktc24a31e0002@edu.kyoto-tech.ac.jp>
2025-02-13 18:28:54 +0900

    HamburgerMenuConpornent

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
+import HamburgerMenu from "./HamburgerMenu";
+
+
 const Header: React.FC = () => {
-    return (
-      <div className="w-screen py-8 bg-black text-center text-2xl font-bold tracking-wider text-white items-center">
-        POKER
+  return (
+    <div className="w-screen py-8 bg-black text-white text-2xl font-bold tracking-wider flex justify-center items-center relative">
+      <div>POKER</div>
+      <div className="absolute right-6">
+        <HamburgerMenu />
       </div>
-    );
-  };
-  
-  export default Header;
-  
-  
+    </div>
+  );
+};
+
+export default Header;
