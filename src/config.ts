d35094163d5a274b2053a39bd48334d503f2d807
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-19 01:12:08 +0900

    gameIDを変更

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = 'http://localhost:8000';
-export const FIXED_GAME_ID = "e211d6c5-0a0e-4ab3-bb23-4643935d85e7";
+export const FIXED_GAME_ID = "0ec3f7d1-567a-4ae4-810f-54bc5e5b0a1a";
