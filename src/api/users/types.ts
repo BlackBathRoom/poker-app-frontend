74c1c71b9805f9937ec423b58b048e5c34c6f7f7
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-19 03:11:59 +0900

    UserDataにidを追加

--- src/api/users/types.ts
+++ src/api/users/types.ts
@@ -1,8 +1,9 @@
 import { Role } from "../../game/types";
 
 export interface UserData {
+    id: string;
     name: string;
     chip: number;
     role: Role | null;
     isplaying: boolean;
-}
+};
