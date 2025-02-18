679d6ce716837930891d2e336c07086d7cbf2f19
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-19 02:40:04 +0900

    ユーザー情報編集モーダルでユーザー情報と更新用関数を受け取るように変更

--- src/components/Admin/UserEditModal.tsx
+++ src/components/Admin/UserEditModal.tsx
@@ -1,50 +1,40 @@
 import { useState } from "react";
 import ModalFrame from "../Modal/ModalFrame";
 import Btn from "../Btn";
+import { UserInfo } from "../../game/types";
 
 type Props = {
-  closeModal: () => void;
+    userInfo: Pick<UserInfo, "name" | "chip">;
+    updateUserInfo: (userInfo: Partial<UserInfo>) => void;
+    closeModal: () => void;
 };
 
-const UserEditModal: React.FC<Props> = ({ closeModal }) => {
-  const [name, setName] = useState("");
-  const [chips, setChips] = useState(0);
+const UserEditModal: React.FC<Props> = ({ userInfo, updateUserInfo, closeModal }) => {
+    const [chips, setChips] = useState(userInfo.chip);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault(); 
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault(); 
+        updateUserInfo({ chip: chips });
+        closeModal();
+    };
 
-    await fetch("/api/updateUser", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ id: 1, name, chips }),
-    });
-
-    closeModal();
-  };
-
-  return (
-    <ModalFrame modalName="ユーザー情報変更" closeModal={closeModal}>
-      <form 
-        className="flex flex-col items-center space-y-4 w-full max-w-xs sm:max-w-sm md:max-w-md p-4 text-black"
-        onSubmit={handleSubmit}
-      >
-        <input 
-          className="w-full p-2 border border-gray-300 rounded-md"
-          placeholder="名前"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input 
-          className="w-full p-2 border border-gray-300 rounded-md"
-          type="number"
-          placeholder="チップ"
-          value={chips}
-          onChange={(e) => setChips(Number(e.target.value))}
-        />
-        <Btn type="submit">更新↻</Btn>
-      </form>
-    </ModalFrame>
-  );
+    return (
+        <ModalFrame modalName={`${userInfo.name}の情報を変更`} closeModal={closeModal}>
+            <form 
+                className="flex flex-col items-center space-y-4 w-full max-w-xs sm:max-w-sm md:max-w-md p-4 text-black"
+                onSubmit={handleSubmit}
+            >
+                <input 
+                className="w-full p-2 border border-gray-300 rounded-md"
+                type="number"
+                placeholder="チップ"
+                value={chips}
+                onChange={(e) => setChips(Number(e.target.value))}
+                />
+                <Btn type="submit">更新↻</Btn>
+            </form>
+        </ModalFrame>
+    );
 };
 
 export default UserEditModal;
