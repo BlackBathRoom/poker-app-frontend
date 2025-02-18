17275c91a675d3b674a2359752831117a1e4ea4b
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-19 03:06:29 +0900

    userListでユーザー情報と更新関数を引数に追加

--- src/components/Admin/UserList.tsx
+++ src/components/Admin/UserList.tsx
@@ -1,35 +1,39 @@
+import { UserInfo } from "../../game/types";
 import UserEdit from "./UserEdit";
 
-type User = {
-    id: number;
-    name: string;
-    chips: number;
-};
+
+type UserInfoWithId = {
+    id: string;
+} & UserInfo;
 
 type Props = {
-    users: User[];
+    users: UserInfoWithId[];
+    updateUserInfo: (id: string, userInfo: Partial<UserInfo>) => void;
 };
 
-const UserList: React.FC<Props> = ({ users }) => {
-  return (
-    <div className="p-4 bg-gray-800 rounded-md max-h-[80%] overflow-y-auto">
-      <h2 className="text-white text-xl text-center mb-2">ユーザー</h2>
-      <ul className="flex flex-col gap-2">
-        {users.map((user) => (
-          <li 
-            key={user.id} 
-            className="flex justify-around  text-white sm:text-xl md:text-2xl lg:text-3xl p-2 border-b border-gray-600 "
-          >
-            <div className="flex justify-center">
-              <UserEdit />
-            </div>
-            <span className="text-center">{user.name}</span>
-            <span className="text-center">{user.chips} </span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+const UserList: React.FC<Props> = ({ users, updateUserInfo }) => {
+    return (
+        <div className="p-4 bg-gray-800 rounded-md max-h-[80%] overflow-y-auto">
+            <h2 className="text-white text-xl text-center mb-2">ユーザー</h2>
+            <ul className="flex flex-col gap-2">
+                {users.map((user) => (
+                    <li
+                        key={user.id} 
+                        className="flex justify-around  text-white sm:text-xl md:text-2xl lg:text-3xl p-2 border-b border-gray-600 "
+                    >
+                        <div className="flex justify-center">
+                            <UserEdit
+                                userInfo={user}
+                                updateUserInfo={(userInfo) => updateUserInfo(user.id, userInfo)} 
+                            />
+                        </div>
+                        <span className="text-center">{user.name}</span>
+                        <span className="text-center">{user.chip}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default UserList;
