ab7224a5266cdfae900d6f984a8afb39b55239e3
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-18 23:35:46 +0900

    mainPageをゲーム情報と紐づけ

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -9,7 +9,7 @@ import ActionBtn from "../components/UserManage/ActionBtn";
 import ActionModal from "../components/UserManage/ActionModal/ActionModal";
 import GameInfo from "../components/GameInfo/GameInfo";
 import UserInfo from "../components/Userinformation/UserInfo";
-import { usePutGameInfo } from "../api/gameInfo";
+import { useGetGameInfo, usePutGameInfo } from "../api/gameInfo";
 import { FIXED_GAME_ID } from "../config";
 
 
@@ -20,7 +20,8 @@ const MainPage: React.FC = () => {
     const navigate = useNavigate();
     if (id === null) navigate("/login", { replace: true });
 
-    const { data, isPending, isError } = useGetUser(id as string);
+    const userQuery = useGetUser(id as string);
+    const gameQuery = useGetGameInfo(FIXED_GAME_ID);
     const userMutate = usePutUserInfo(id as string);
     const gameMutate = usePutGameInfo(FIXED_GAME_ID);
 
@@ -28,19 +29,19 @@ const MainPage: React.FC = () => {
     const game = useGame();
 
     const action = (actionType: ActionType, amount: number) => {
-        if (!data) throw new Error("ユーザー情報が取得できません");
+        if (!userQuery.data) throw new Error("ユーザー情報が取得できません");
        const update = game.action(
             actionType,
             { currentBet: 3, pot: 3 },
-            { chip: data.chip, amount },
+            { chip: userQuery.data.chip, amount },
         );
         userMutate.mutate(update.userInfo);
         gameMutate.mutate(update.gameInfo);
     };
 
-    if (isPending) return <div>Loading...</div>;
-    if (isError) return <div>Error</div>;
-    if (!data) return <div>No data</div>;
+    if (userQuery.isPending || gameQuery.isPending) return <div>Loading...</div>;
+    if (userQuery.isError || userQuery.isError) return <div>Error</div>;
+    if (!userQuery.data || !gameQuery.data) return <div>No data</div>;
 
     return (
         <div className="h-full w-full flex flex-col items-center justify-center">
@@ -51,8 +52,12 @@ const MainPage: React.FC = () => {
                 <div className="bg-white text-gray-900 p-8 sm:p-10 md:p-12 rounded-lg shadow-md text-center 
                                 w-[90%] sm:max-w-[600px] md:max-w-[700px] lg:max-w-[850px] min-h-[75vh] 
                                 flex flex-col gap-6 justify-between">
-                    <GameInfo potSize={256} rate={25600} />
-                    <UserInfo userName={data.name} chips={data.chip} isPlaying={data.isPlaying} />
+                    <GameInfo potSize={gameQuery.data.pot} rate={gameQuery.data.currentBet} />
+                    <UserInfo
+                        userName={userQuery.data.name}
+                        chips={userQuery.data.chip}
+                        isPlaying={userQuery.data.isPlaying}
+                    />
                     <ActionBtn handleModal={openModal} isPlaying={true} />
                 </div>
             </div>
