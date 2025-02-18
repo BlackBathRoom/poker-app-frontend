ca91e214f70a285e333b143befa1e2122ccad7b7
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-19 02:21:03 +0900

    インデント幅の修正

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -3,28 +3,28 @@ import UserList from "../components/Admin/UserList";
 import GameControl from "../components/Admin/GameControl";
 
 type User = {
-  id: number;
-  name: string;
-  chips: number;
+    id: number;
+    name: string;
+    chips: number;
 };
 
 const AdminPage: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([
-    { id: 1, name: "SampleUser", chips: 1000 }
-  ]);
+    const [users, setUsers] = useState<User[]>([
+        { id: 1, name: "SampleUser", chips: 1000 }
+    ]);
 
-  // useEffect(() => {
-  //   fetch("/api/users")
-  //     .then((res) => res.json())
-  //     .then((data) => setUsers(data));
-  // }, []);
+    // useEffect(() => {
+    //   fetch("/api/users")
+    //     .then((res) => res.json())
+    //     .then((data) => setUsers(data));
+    // }, []);
 
-  return (
-    <div className="p-10">
-      <UserList users={users} />
-      <GameControl />
-    </div>
-  );
+    return (
+        <div className="p-10">
+            <UserList users={users} />
+            <GameControl />
+        </div>
+    );
 };
 
 export default AdminPage;
