5a37a4bc209b03f33a2cf6fbe4710c10cfe02302
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-19 23:41:49 +0900

    admin用のクエリキーを定義

--- src/api/users/key.ts
+++ src/api/users/key.ts
@@ -1,4 +1,5 @@
 export const usersKeys = {
     all: ["users"] as const,
     id: (userId: string) => [...usersKeys.all, userId] as const,
+    allWithId: () => [...usersKeys.all, "ids"] as const,
 } as const;
