2bad9c2671c75ee50dc2fad332bf02cd6dec689f
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-20 00:16:54 +0900

    管理者ページでバックエンドからデータを取得

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,24 +1,16 @@
-import { useState } from "react";
 import UserList from "../components/Admin/UserList";
 import GameControl from "../components/Admin/GameControl";
+import { useGetAllUserWithId } from "../api/users";
 
-type User = {
-    id: number;
-    name: string;
-    chips: number;
-};
 
 const AdminPage: React.FC = () => {
-    const [users, setUsers] = useState<User[]>([
-        { id: 1, name: "SampleUser", chips: 1000 }
-    ]);
+    const userQuery = useGetAllUserWithId();
 
-    // useEffect(() => {
-    //   fetch("/api/users")
-    //     .then((res) => res.json())
-    //     .then((data) => setUsers(data));
-    // }, []);
+    if (userQuery.isPending) return <div>Loading...</div>;
+    if (userQuery.isError) return <div>Error</div>;
+    if (!userQuery.data) return <div>No data</div>;
 
+    const users = userQuery.data
     return (
         <div className="p-10">
             <UserList users={users} />
