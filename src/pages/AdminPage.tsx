85d4764416474c83e39746a6838569d0b65d4ac9
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-20 02:49:10 +0900

    AdminPageをバックエンドと接続

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -2,24 +2,65 @@ import UserList from "../components/Admin/UserList";
 import GameControl from "../components/Admin/GameControl";
 import { useGetAllUserWithId, usePutSelectedUserInfo } from "../api/users";
 import { UserInfo } from "../game/types";
+import { useGetGameInfo, usePutGameInfo } from "../api/gameInfo";
+import { FIXED_GAME_ID } from "../config";
 
 
 const AdminPage: React.FC = () => {
     const userQuery = useGetAllUserWithId();
-    const userMutate = usePutSelectedUserInfo();
+    const gameQuery = useGetGameInfo(FIXED_GAME_ID);
 
-    if (userQuery.isPending) return <div>Loading...</div>;
-    if (userQuery.isError) return <div>Error</div>;
-    if (!userQuery.data) return <div>No data</div>;
+    const userMutate = usePutSelectedUserInfo();
+    const gameMutate = usePutGameInfo(FIXED_GAME_ID);
+
+    if (userQuery.isPending || gameQuery.isPending) return <div>Loading...</div>;
+    if (userQuery.isError || gameQuery.isError) return <div>Error</div>;
+    if (!userQuery.data || !gameQuery.data) return <div>No data</div>;
 
     const updateUserInfo = (id: string, userInfo: Partial<UserInfo>) => {
         userMutate.mutate({ ids: [id], userInfo });
     };
 
+    const startGame = () => {
+        if (!userQuery.data || !gameQuery.data) return;
+        userMutate.mutate({
+            ids: userQuery.data.map((user) => user.id),
+            userInfo: { isPlaying: true },
+        });
+        gameMutate.mutate({ "isPlaying": true });
+    };
+
+    const endGame = (id: string) => {
+        if (!userQuery.data || !gameQuery.data) return;
+        userMutate.mutate({
+            ids: [id],
+            userInfo: { chip: gameQuery.data.pot },
+        });
+
+        userMutate.mutate({
+            ids: userQuery.data.map((user) => user.id),
+            userInfo: { isPlaying: false },
+        });
+        gameMutate.mutate({
+            "currentBet": 0,
+            "pot": 0,
+            "isPlaying": false,
+        });
+    };
+
     return (
         <div className="p-10">
             <UserList users={userQuery.data} updateUserInfo={updateUserInfo} />
-            <GameControl />
+            <GameControl
+                users={
+                    userQuery.data.map((user) => ({
+                        id: user.id,
+                        name: user.name,
+                    }))
+                }
+                startGame={startGame}
+                endGame={endGame}
+            />
         </div>
     );
 };
