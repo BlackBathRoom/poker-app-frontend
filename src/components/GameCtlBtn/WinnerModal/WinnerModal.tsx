3223eb8718ce7be9c62562b770b252a53aa1e52c
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-20 02:26:38 +0900

    WinnerMadalで引数を変更

--- src/components/GameCtlBtn/WinnerModal/WinnerModal.tsx
+++ src/components/GameCtlBtn/WinnerModal/WinnerModal.tsx
@@ -1,26 +1,28 @@
+import { UserInfoWithId } from "../../../game/types";
 import Btn from "../../Btn";
 import ModalFrame from "../../Modal/ModalFrame";
 
+
 type Props = {
-    userNames: string[];
-    endGame: (index: number) => void;
+    users: Pick<UserInfoWithId, "id" | "name">[];
+    endGame: (id: string) => void;
     closeModal: () => void;
 };
 
-const WinnerModal: React.FC<Props> = ({ userNames, endGame, closeModal}) => {
+const WinnerModal: React.FC<Props> = ({ users, endGame, closeModal}) => {
     return (
         <ModalFrame modalName="Result" closeModal={closeModal}>
             <ul className="flex flex-col items-center">
-                {userNames.map((name, index) => (
-                    <li key={index} className="text-2xl text-zinc-300 my-2">
+                {users.map((user) => (
+                    <li key={user.id} className="text-2xl text-zinc-300 my-2">
                         <Btn
-                            onClick={() => endGame(index)}
+                            onClick={() => endGame(user.id)}
                             className="w-32 h-10 m-auto"
                             bgColor="bg-emerald-300"
                             hoverBgColor="bg-emerald-400"
                         >
                             <span className="text-zinc-900 text-xl font-extrabol p-2">
-                                {name}
+                                {user.name}
                             </span>
                         </Btn>
                     </li>
