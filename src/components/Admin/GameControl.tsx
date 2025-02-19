227f83ecc672e0616d82707a1a1fb87463d00b0a
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-20 02:38:30 +0900

    GameControlで実装の変更に対応

--- src/components/Admin/GameControl.tsx
+++ src/components/Admin/GameControl.tsx
@@ -2,25 +2,37 @@ import { useModal } from "../Modal/useModal";
 import ModalFrame from "../Modal/ModalFrame";
 import GameStartBtn from "../GameCtlBtn/GameStartBtn";
 import GameEndBtn from "../GameCtlBtn/GameEndBtn";
+import { UserInfoWithId } from "../../game/types";
 
-const GameControl = () => {
-  const { Modal, openModal, closeModal } = useModal();
 
-  return (
-    <>
-      <button 
-        onClick={openModal} 
-        className="bg-gray-800 hover:bg-gray-600 px-4 py-2 mt-4 text-white rounded-md w-fit mx-auto block">
-        ゲーム制御
-      </button>
-      <Modal>
-        <ModalFrame modalName="ゲーム管理" closeModal={closeModal}>
-            <GameStartBtn startGame={() => alert("ゲーム開始")} />
-            <GameEndBtn userNames={["Player1", "Player2"]} endGame={(index) => alert(`ゲーム終了: ${index}`)} />
-        </ModalFrame>
-      </Modal>
-    </>
-  );
+type Props = {
+    users: Pick<UserInfoWithId, "id" | "name">[];
+    startGame: () => void;
+    endGame: (id: string) => void;
+};
+
+const GameControl: React.FC<Props> = ({
+    users,
+    startGame,
+    endGame,
+}) => {
+    const { Modal, openModal, closeModal } = useModal();
+
+    return (
+        <>
+        <button 
+            onClick={openModal} 
+            className="bg-gray-800 hover:bg-gray-600 px-4 py-2 mt-4 text-white rounded-md w-fit mx-auto block">
+            ゲーム制御
+        </button>
+        <Modal>
+            <ModalFrame modalName="ゲーム管理" closeModal={closeModal}>
+                <GameStartBtn startGame={startGame} />
+                <GameEndBtn users={users} endGame={endGame} />
+            </ModalFrame>
+        </Modal>
+        </>
+    );
 };
 
 export default GameControl;
