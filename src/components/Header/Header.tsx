0e384272748a8d5d364950810bf8a1dd962ee4ff
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-23 05:42:21 +0900

    ヘッダー内でサイドメニューの開閉を管理

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import HamburgerMenu from "./HamburgerMenu";
+import SideMenu from "../SideMenu/SideMenu";
 
 
 const Header: React.FC = () => {
+  const [ isOpen,setIsOpen ] = useState<boolean>(false);
+
   return (
-    <div className="w-screen py-8 bg-black text-white text-2xl font-bold tracking-wider flex justify-center items-center relative">
+    <>
+    <div className="w-full py-8 bg-black text-white text-2xl font-bold tracking-wider flex justify-center items-center relative z-50">
       <div>POKER</div>
       <div className="absolute right-6">
-        <HamburgerMenu />
+        <HamburgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
     </div>
+    <SideMenu isOpen={isOpen} />
+    </>
   );
 };
 
