dcfc9aa5d4e320e1637f3ba1b9af333f5be3ec3c
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-23 06:01:05 +0900

    growからcalcで計算する形に変更
    無理矢理なのでいい方法あればそれで修正する

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -5,10 +5,10 @@ import Header from "./components/Header/Header";
 const Layout: React.FC = () => {
     return (
         <div className="h-dvh w-screen bg-sky-200 flex flex-col">
-            <div className="w-full">
+            <div className="w-full h-24">
                 <Header/>
             </div>
-            <div className="flex-grow w-full h-fit px-2 py-3">
+            <div className="flex-grow w-full h-[calc(100dvh-96px)] px-2 py-3">
                 <Outlet />
             </div>
         </div>
