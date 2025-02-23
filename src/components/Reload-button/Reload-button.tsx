7227e03b05ff4e1fcbd20a6b719ecd52d0506d8e
haradatomoki0512 <ktc24a31e0012@edu.kyoto-tech.ac.jp>
2025-02-23 14:30:05 +0900

    関数受け取り追加

--- src/components/Reload-button/Reload-button.tsx
+++ src/components/Reload-button/Reload-button.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-const ReloadButton: React.FC = () => {
+interface ReloadButtonProps {
+  onReload: () => void;
+}
+
+const ReloadButton: React.FC<ReloadButtonProps> = ({ onReload }) => {
   return (
     <button
+      onClick={onReload}
       className="px-6 py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-semibold 
                  text-lg rounded-full shadow-lg hover:opacity-80 active:scale-95 
                  transition duration-300 ease-in-out"
