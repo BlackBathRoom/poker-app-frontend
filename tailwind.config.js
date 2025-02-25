f980e03f4c2d8279b1b3912233a70367cd0f1db2
InoueToranosuke <ktc24a31e0002@edu.kyoto-tech.ac.jp>
2025-02-26 00:53:25 +0900

    tailwind.config.js アニメーション追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+    theme: {
+        extend: {
+            animation: {
+                "circle-spin": "circleSpin 10s linear infinite",
+            },
+            keyframes: {
+                circleSpin: {
+                    "0%": { transform: "rotate(0deg)" },
+                    "100%": { transform: "rotate(360deg)" },
+                },
+            },
+        },
+    },
   plugins: [],
 }
 
