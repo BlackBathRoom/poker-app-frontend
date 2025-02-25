e26786a3beecee288eb584246f4681cb317ebcf5
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-26 00:58:09 +0900

    ロール変更関数の挙動を修正

--- src/game/changeRole.ts
+++ src/game/changeRole.ts
@@ -9,27 +9,24 @@ type Props = {
 };
 
 export const changeRole = ({ users, newDBIndex }: Props): RoleAndBlank[] => {
+    let isComplete = false;
     let i = newDBIndex;
     let role: RoleAndBlank = "DB";
     const newUsers: RoleAndBlank[] = users.map(() => "");
 
-    while (true) {
-        try {
-            if (newUsers[i] === "DB") break;
-            newUsers[i] = role;
+    while (!isComplete) {
+        if (i >= users.length) i = 0;
+        newUsers[i] = role;
 
-            if (role === "DB") {
-                role = "SB";
-            } else if (role === "SB") {
-                role = "BB";
-            } else {
-                role = "";
-            }
-
-            i++;
-        } catch {
-            i = 0;
+        if (role === "DB") {
+            role = "SB";
+        } else if (role === "SB") {
+            role = "BB";
+        } else if (role === "BB") {
+            isComplete = true;
         }
+    
+        i++;
     };
 
     return newUsers;
