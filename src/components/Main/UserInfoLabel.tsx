e9f45091e9b2253eeca6a4391280f5629768b64d
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-26 02:04:33 +0900

    UserInfoを受け取るように変更し、role用のアイコンを表示

--- src/components/Main/UserInfoLabel.tsx
+++ src/components/Main/UserInfoLabel.tsx
@@ -1,20 +1,23 @@
+import { UserInfo } from "../../game/types";
+import RoleIcon from "../RoleIcon";
+
+
 interface Props {
-    userName: string;
-    chips: number;
-    isPlaying?: boolean; 
+    userInfo: UserInfo;
 };
 
-const UserInfoLabel: React.FC<Props> = ({ userName, chips, isPlaying = true }) => { 
+const UserInfoLabel: React.FC<Props> = ({ userInfo }) => { 
     return (
         <div className="flex flex-col gap-4 p-8 bg-white/40 items-center rounded-md shadow">
+            <div className="text-2xl flex gap-2 items-center sm:text-3xl md:text-4xl lg:text-4xl">
+                <RoleIcon role="" />
+                <span className="font-bold text-black pb-1">{userInfo.name}</span>
+            </div>
             <p className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl">
-                <span className="font-bold text-black">{userName}</span>
+                チップ : <span className="font-bold text-black">{userInfo.chip} 枚</span>
             </p>
-            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl">
-                チップ : <span className="font-bold text-black">{chips} 枚</span>
-            </p>
-            <p className={`text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold ${isPlaying ? 'text-green-500' : 'text-red-500'}`}>
-                {isPlaying ? '参加中' : '不参加'}
+            <p className={`text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold ${userInfo.isPlaying ? 'text-green-500' : 'text-red-500'}`}>
+                {userInfo.isPlaying ? '参加中' : '不参加'}
             </p>
         </div>
     );
